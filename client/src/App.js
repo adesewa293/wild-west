4f86e8fc0e038338629135e5a5dcefe14efece4a
Adesewa Aishat <adesewaaishat@gmail.com>
2023-07-27 14:28:07 +0100

    add client code

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  const [outcome, setOutcome] = useState("");
+  
+  async function handleSurvive() {
+    const API = "http://localhost:8080/survive";
+    const res = await axios.get(API);
+    setOutcome(res.data);
+     console.log('outcome', outcome)
+    const firstWord = res.data.replace(/ .*/, "");
+    console.log("firstWord", firstWord);
+  }
+
+  return (
+    <div className="App">
+
+      <button onClick={handleSurvive}>Brave the oregon trail!</button>
+      {outcome}
+      <br />
+    </div>
+  );
+}
+
+export default App;
